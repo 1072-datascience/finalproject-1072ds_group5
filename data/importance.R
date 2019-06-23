0bda554bb7376afc316da80c066dc641d10d99e5
tsaihanlung841203 <tsaihanlung841203@gmail.com>
2019-06-24 02:16:59 +0800

    graph x fix
    graph x fix

--- data/importance.R
+++ data/importance.R
@@ -2,8 +2,18 @@ library(Boruta)
 train <- read.csv("fb_data.csv",header = TRUE)
 summary(train)
 
-boruta.train <- Boruta(y_comments~., data=train, ntree=100, doTrace=2, maxRuns = 15, pValue = 0.01)
+boruta.train <- Boruta(y_comments~., data=train, ntree=100, doTrace=2, maxRuns = 200, pValue = 0.01)
 print(boruta.train)
-plot(boruta.train)
+#plot(boruta.train)
+
+plot(boruta.train, xlab="", xaxt="n")
+
+lz<-lapply(1:ncol(boruta.train$ImpHistory),function(i)
+  boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
+names(lz)<-colnames(boruta.train$ImpHistory)
+Labels<-sort(sapply(lz,median))
+axis(side=1, las=2, labels = names(Labels),
+     at = 1:ncol(boruta.train$ImpHistory), cex.axis = 0.7)
+
 
 #TentativeRoughFix(boruta.train)
